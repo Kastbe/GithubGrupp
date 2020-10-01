3602ae8d35d9eb25f6d05a003351433c9e58c429
Kastbe <67763895+Kastbe@users.noreply.github.com>
2020-10-01 13:41:52 +0200

    Summa

--- Grupp4/Grupp4/Program.cs
+++ Grupp4/Grupp4/Program.cs
@@ -7,11 +7,17 @@ namespace Grupp4
         static void Main(string[] args)
         {
             int number1 = 5;
+
             //onur
             int number2 = 4;
 
             // Niclas
             int number3 = 3;
+
+            int totalNumber = number1 + number2 + number3;
+
+            //Kalle
+            Console.WriteLine("Summa: " + totalNumber);
         }
     }
 }
